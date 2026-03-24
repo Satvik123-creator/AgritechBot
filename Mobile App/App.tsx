0b0bc060823439a2239815091c1ca26157e8572a
Ashish Nith <ashishnith@users.noreply.github.com>
2026-03-24 19:35:26 +0530

    modified:   Mobile App/App.tsx

--- Mobile App/App.tsx
+++ Mobile App/App.tsx
@@ -1,7 +1,19 @@
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+
 import { RootNavigator } from './src/navigation/RootNavigator';
 import { AppProviders } from './src/providers/AppProviders';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+    ...MaterialCommunityIcons.font,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <AppProviders>
       <RootNavigator />
