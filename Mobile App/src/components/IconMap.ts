537e3f818b79e0b37ae0e3f40f2bafd44134ca77
Ashish Nith <ashishnith@users.noreply.github.com>
2026-03-30 20:58:55 +0530

    Update

--- Mobile App/src/components/IconMap.ts
+++ Mobile App/src/components/IconMap.ts
@@ -7,7 +7,7 @@ import {
   X, Scan, Brain, ArrowLeft, Share2, ChevronLeft, ChevronRight,
   Globe, Mail, Camera, CloudSun, Droplets,
   MoreVertical, PlayCircle, ImagePlus, LocateFixed, Locate, MapPinOff, MapPin,
-  Minus, Plus, Trash2, CheckCircle2, CircleCheckBig
+  Minus, Plus, Trash2, CheckCircle2, CircleCheckBig, Send
 } from 'lucide-react-native';
 
 export const IconMap: Record<string, any> = {
@@ -19,5 +19,5 @@ export const IconMap: Record<string, any> = {
   X, Scan, Brain, ArrowLeft, Share2, ChevronLeft, ChevronRight,
   Globe, Mail, Camera, CloudSun, Droplets,
   MoreVertical, PlayCircle, ImagePlus, LocateFixed, Locate, MapPinOff, MapPin,
-  Minus, Plus, Trash2, CheckCircle2, CircleCheckBig
+  Minus, Plus, Trash2, CheckCircle2, CircleCheckBig, Send
 };
